8fccf6e8a80aed5dd9688c002efaa80de0bd2287
Amritendra <110092063+Amritendra@users.noreply.github.com>
2022-07-31 14:04:57 +0530

    basics

---
+++ datatypes.py
@@ -0,0 +1,50 @@
+x=1920
+y=5646
+x2=-68557575
+y2=-67656
+y3=89.8e399
+x4="amrit"
+x5="digipodioum"
+y5='colur'
+x6=898.767
+x7=None
+x8=0
+print(x,type(x))
+print(y,type(y))
+print(x2,type(x2))
+print(y2,type(y2))
+print(y3,type(y3))
+print(x4,type(x4))
+print(x5,type(x5))
+print(y5,type(y5))
+print(x6,type(x6))
+print(x7,type(x7))
+print(x8,type(x8))
+
+'''list are mutable'''
+
+a=[5,3,7,56,56,76,87]
+vals=[23,676.88,'amrit']
+colors=['red','green','yellow','white']
+print(a,type(a))
+print(vals,type(vals))
+print(colors,type(colors))
+
+''' tuples are immutable'''
+
+v=(76,5445,7.8,98.8,"amritsingh")
+print(v,type(v))
+
+''' set'''
+v1={4,5,4,4,4,6656,7676,.767,9.9,9.9}
+print(v1,type(v1))
+
+
+''' dictionaries '''
+car_info={"brand":"ford","model":"mustamg","year":'1965'}
+print(car_info,type(car_info))
+
+l=['apple','banana','papaya','palm']
+print('papaya' in l)
+print('mango' not in l)
+
---
+++ operaters.py
@@ -0,0 +1,14 @@
+a=176
+b=6
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a%b)
+print(a/b)
+print(a//b)
+print(a**b)
+
+l=['apple','banana','papaya','palm']
+print('papaya' in l)
+print('mango' not in l)
