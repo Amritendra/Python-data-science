eae584346659f2a9c8d0bb5b413767a2c8797be5
Amritendra <110092063+Amritendra@users.noreply.github.com>
2022-08-19 15:35:27 +0530

    practice

---
+++ createptrn.py
@@ -0,0 +1,26 @@
+num=[1,2,3,4,5,6,8,10,12,15,16,20,25]
+for i in num:
+    if i<=5:
+        print(i,end=" ")
+print("\n")
+for i in range(1,11):
+    if i%2==0:
+        
+        print(i,end=" ")
+print("\n")
+
+for i in range(2,16):
+    if i%3==0:
+        print(i,end=" ")
+print("\n")
+
+for i in range(3,21):
+    if i%4==0:
+        print(i,end=" ")
+print("\n")
+
+for i in range(4,26):
+    if i%5==0:
+        print(i,end=" ")
+
+
